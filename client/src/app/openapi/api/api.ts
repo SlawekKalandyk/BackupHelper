9bc827c0e999ac7d49bb16be971a3666338f11b4
Slawomir Kalandyk <slawomir.kalandyk@gmail.com>
2023-06-28 15:54:55 +0200

    Remove angular client

--- client/src/app/openapi/api/api.ts
+++
@@ -1,4 +0,0 @@
-export * from './backup.service';
-import {BackupService} from './backup.service';
-
-export const APIS = [BackupService];
--- client/src/app/openapi/model/models.ts
+++
@@ -1,5 +0,0 @@
-export * from './backupConfiguration';
-export * from './backupDirectory';
-export * from './backupDto';
-export * from './backupFile';
-export * from './configurationSaveDto';
