6146fe0c6f7ab3e8df5ac4a87f2f012dd4e1f63e
Slawomir Kalandyk <slawomir.kalandyk@gmail.com>
2023-06-28 19:00:03 +0200

    Add date based naming for backups

--- BackupHelper/BackupHelper.ConsoleApp/Program.cs
+++ BackupHelper/BackupHelper.ConsoleApp/Program.cs
@@ -4,7 +4,6 @@ using BackupHelper.Core.FileZipping;
 using MediatR;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 
 namespace BackupHelper.ConsoleApp
 {
@@ -19,12 +18,54 @@ namespace BackupHelper.ConsoleApp
                 .AddCoreServices(configuration)
                 .BuildServiceProvider();
 
-            var backupSavePath = args[0];
+            if (args.Length != 2 || string.IsNullOrEmpty(args[0]) || string.IsNullOrEmpty(args[1]))
+            {
+                throw new ArgumentException();
+            }
+
+            var backupSavePath = GetBackupSavePath(args[0]);
             var backupConfigPath = args[1];
             var backupConfiguration = BackupConfiguration.FromJsonFile(backupConfigPath);
 
             var mediator = services.GetRequiredService<IMediator>();
             await mediator.Send(new CreateBackupCommand(backupConfiguration, backupSavePath));
+        }
+
+        private static string GetBackupSavePath(string argPath)
+        {
+            if (File.Exists(argPath))
+            {
+                return argPath;
+            }
+
+            if (Directory.Exists(argPath))
+            {
+                return Path.Combine(argPath, CreateDateTimeBasedZipFileName());
+            }
+
+            if (Path.HasExtension(argPath))
+            {
+                var directories = Path.GetDirectoryName(argPath);
+                Directory.CreateDirectory(directories);
+                return argPath;
+            }
+
+            Directory.CreateDirectory(argPath);
+            return Path.Combine(argPath, CreateDateTimeBasedZipFileName());
+        }
+
+        private static string CreateDateTimeBasedZipFileName()
+        {
+            var time = DateTime.Now;
+            var baseFileName = $"{time:yyyy-MM-dd_HH-mm-ss}_backup";
+            var fileName = $"{baseFileName}.zip";
+            var i = 1;
+            while (File.Exists(fileName))
+            {
+                fileName = baseFileName + $".{i}.zip";
+                ++i;
+            }
+            return fileName;
         }
     }
 }
